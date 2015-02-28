df1d2e31d33ab461545d06be7510f6d63b53dca3
freddie <fredy.mendivelso@hotmail.com>
2015-02-28 09:47:25 -0800

    Agregar archivos de ejemplos de la configuracion para crear formularios

---
+++ www/conf/conf.php
@@ -0,0 +1,47 @@
+<?php
+
+// Fecha y hora del sistema" . FIN_DE_LINEA;
+date_default_timezone_set('America/Bogota');
+
+define("RUTA_GENERADOR_CODIGO", "../zc");
+/**
+ * Tipos de elementos aceptados
+ */
+define("ZC_ID", "id");
+define("ZC_TIPO_WS", "tipo_de_servicio");
+define("ZC_FORMULARIO_NOMBRE", "nombre_formulario");
+define("ZC_FORMULARIO_METODO", "se_envia");
+define("ZC_NOMBRE_ACCION_CLIENTE", "nombre_accion");
+
+define("ZC_WS_SOAP", "soap");
+define("ZC_WS_REST", "rest");
+
+define("ZC_ELEMENTO_CAJA_TEXTO", "caja");
+define("ZC_ELEMENTO_BOTON", "boton");
+define("ZC_ELEMENTO_RADIO", "radio");
+define("ZC_ELEMENTO_CHECKBOX", "checkbox");
+define("ZC_ELEMENTO_RESTABLECER", "restablecer");
+define("ZC_ELEMENTO_TIPO", "que_quieres");
+define("ZC_ELEMENTO_ETIQUETA", "como_se_llama");
+
+define("ZC_DATO", "que_dato_recibira");
+define("ZC_DATO_NUMERICO", "numeros");
+define("ZC_DATO_EMAIL", "correo");
+define("ZC_DATO_FECHA", "fecha");
+define("ZC_DATO_URL", "url");
+define("ZC_DATO_ALFANUMERICO", "alfanumerico");
+define("ZC_DATO_MENSAJE_ERROR", "mensaje_de_error_tipo_dato");
+define("ZC_DATO_MENSAJE_ERROR_PREDETERMINADO", "Tipo de dato incorrecto");
+
+define("ZC_LONGITUD_MAXIMA", "longitud_maxima_es");
+define("ZC_LONGITUD_MAXIMA_ERROR", "mensaje_de_error_longitud_maxima");
+define("ZC_LONGITUD_MAXIMA_ERROR_PREDETERMINADO", "Supera la longitud permitida (&[Longitud]&)");
+define("ZC_LONGITUD_MINIMA", "longitud_minima_es");
+define("ZC_LONGITUD_MINIMA_ERROR", "mensaje_de_error_longitud_minima");
+define("ZC_LONGITUD_MINIMA_ERROR_PREDETERMINADO", "Se esperan al menos (&[Longitud]&) caracteres");
+
+define("ZC_OBLIGATORIO", "es_obligatorio");
+define("ZC_OBLIGATORIO_SI", "si");
+define("ZC_OBLIGATORIO_NO", "no");
+define("ZC_OBLIGATORIO_ERROR", "mensaje_de_error_es_obligatorio");
+define("ZC_OBLIGATORIO_ERROR_PREDETERMINADO", "Campo obligatorio");
---
+++ www/conf/crearFormulario.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+require 'conf.php';
+require RUTA_GENERADOR_CODIGO . '/includes/libreria.inc.php';
+require RUTA_GENERADOR_CODIGO . '/modelo/plantilla.class.php';
+require RUTA_GENERADOR_CODIGO . '/modelo/formulario.class.php';
+require RUTA_GENERADOR_CODIGO . '/modelo/procesarXML.class.php';
+
+error_reporting(E_ALL);
+
+/**
+ * Description of crearFormulario
+ *
+ * @author root
+ */
+try {
+    /**
+     * JQuery homologado
+     */
+    $jqueryjs = new plantilla();
+    $jqueryjs->cargarPlantilla(RUTA_GENERADOR_CODIGO . '/plantilla/js/jquery.v2.1.1.min.js');
+    $jqueryjs->crearPlantilla('../publico/js', 'js');
+
+    /**
+     * Parsley Homologado
+     */
+    $parsleyjs = new plantilla();
+    $parsleyjs->cargarPlantilla(RUTA_GENERADOR_CODIGO . '/plantilla/js/parsley.v2.0.5.js');
+    $parsleyjs->crearPlantilla('../publico/js', 'js');
+
+    /**
+     * CSS Homologado
+     */
+    $bootstrapcss = new plantilla();
+    $bootstrapcss->cargarPlantilla(RUTA_GENERADOR_CODIGO . '/plantilla/css/bootstrap.min.css');
+    $bootstrapcss->crearPlantilla('../publico/css', 'css');
+
+    /**
+     * CSS Homologado
+     */
+    $parsleycss = new plantilla();
+    $parsleycss->cargarPlantilla(RUTA_GENERADOR_CODIGO . '/plantilla/css/parsley.css');
+    $parsleycss->crearPlantilla('../publico/css', 'css');
+    /**
+     * Opciones de formulario
+     */
+    $nf = new procesarXML();
+    $nf->cargarArchivosXML('xml');
+} catch (Exception $e) {
+    die($e->getMessage());
+}
