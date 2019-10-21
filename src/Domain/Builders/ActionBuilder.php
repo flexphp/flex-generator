4771abdf398b528c7ba94419ddc9d0fd91cfc0da
Freddie Gar <freddie.gar@outlook.com>
2019-10-21 13:35:13 -0500

    Updated action builder

--- src/Domain/Builders/ActionBuilder.php
+++ src/Domain/Builders/ActionBuilder.php
@@ -2,8 +2,26 @@
 
 namespace FlexPHP\Generator\Domain\Builders;
 
+use Symfony\Component\HttpFoundation\Request;
+
 class ActionBuilder extends AbstractBuilder
 {
+    public function __construct(array $data, array $config = [])
+    {
+        $data['action'] = $data['action'] ?? 'index';
+        $action = strtolower($data['action']);
+
+        if (empty($data['route'])) {
+            $data['route'] = $this->getGuessRoute($action);
+        }
+
+        if (empty($data['methods'])) {
+            $data['methods'] = $this->getGuessMethod($action);
+        }
+
+        parent::__construct($data, $config);
+    }
+
     public function getFileTemplate(): string
     {
         return 'Action.php.twig';
@@ -12,5 +30,31 @@ class ActionBuilder extends AbstractBuilder
     public function getPathTemplate(): string
     {
         return \sprintf('%1$s/Symfony/v43/src/Controller', parent::getPathTemplate());
+    }
+
+    private function getGuessMethod($action)
+    {
+        $method = Request::METHOD_GET;
+
+        switch($action) {
+            case 'create':
+                $method = Request::METHOD_POST;
+                break;
+        }
+
+        return $method;
+    }
+
+    private function getGuessRoute($action)
+    {
+        $route = '/' . $action;
+
+        switch($action) {
+            case 'index':
+                $route = '/';
+                break;
+        }
+
+        return $route;
     }
 }
