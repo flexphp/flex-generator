5493a6fe32949c26efab8e9733de31141c7ebfb5
Freddie Gar <freddie.gar@outlook.com>
2020-03-29 14:48:16 -0500

    Added response file use case test

---
+++ tests/Domain/UseCases/CreateResponseFileUseCaseTest.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of FlexPHP.
+ *
+ * (c) Freddie Gar <freddie.gar@outlook.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace FlexPHP\Generator\Tests\Domain\UseCases;
+
+use FlexPHP\Generator\Domain\Messages\Requests\CreateResponseFileRequest;
+use FlexPHP\Generator\Domain\Messages\Responses\CreateResponseFileResponse;
+use FlexPHP\Generator\Domain\UseCases\CreateResponseFileUseCase;
+use FlexPHP\Generator\Tests\TestCase;
+use FlexPHP\Schema\Schema;
+use FlexPHP\UseCases\Exception\NotValidRequestException;
+
+final class CreateResponseFileUseCaseTest extends TestCase
+{
+    public function testItNotValidRequestThrowException(): void
+    {
+        $this->expectException(NotValidRequestException::class);
+
+        $useCase = new CreateResponseFileUseCase();
+        $useCase->execute(null);
+    }
+
+    /**
+     * @dataProvider getEntityFile()
+     */
+    public function testItSymfony43Ok(string $schemafile, array $expectedFiles): void
+    {
+        $schema = Schema::fromFile($schemafile);
+        $actions = [
+            'R' => 'read',
+            'D' => 'delete',
+        ];
+
+        $request = new CreateResponseFileRequest(
+            $schema->name(),
+            $schema->attributes(),
+            $actions,
+            $this->getOutputFolder()
+        );
+
+        $useCase = new CreateResponseFileUseCase();
+        $response = $useCase->execute($request);
+
+        $this->assertInstanceOf(CreateResponseFileResponse::class, $response);
+        $this->assertEquals(\count($actions), \count($response->files));
+
+        foreach ($response->files as $index => $file) {
+            $filename = \explode('/', $file);
+            $this->assertEquals($expectedFiles[$index], \array_pop($filename));
+            $this->assertFileExists($file);
+
+            \unlink($file);
+        }
+    }
+
+    public function getEntityFile(): array
+    {
+        return [
+            [\sprintf('%1$s/../../Mocks/yaml/posts.yaml', __DIR__), [
+                'ReadPostResponse.php',
+                'DeletePostResponse.php',
+            ]],
+            [\sprintf('%1$s/../../Mocks/yaml/comments.yaml', __DIR__), [
+                'ReadCommentResponse.php',
+                'DeleteCommentResponse.php',
+            ]],
+        ];
+    }
+}
