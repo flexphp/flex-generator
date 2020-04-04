631229b1ce768b8905e68ff80abd5c7cf1a51a0e
Freddie Gar <freddie.gar@outlook.com>
2020-04-04 17:31:07 -0500

    Added create action templates for symfony

---
+++ src/Domain/UseCases/CreateTemplateFileUseCase.php
@@ -0,0 +1,65 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of FlexPHP.
+ *
+ * (c) Freddie Gar <freddie.gar@outlook.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace FlexPHP\Generator\Domain\UseCases;
+
+use FlexPHP\Generator\Domain\Builders\Template\TemplateBuilder;
+use FlexPHP\Generator\Domain\Messages\Requests\CreateTemplateFileRequest;
+use FlexPHP\Generator\Domain\Messages\Responses\CreateTemplateFileResponse;
+use FlexPHP\Generator\Domain\Traits\InflectorTrait;
+use FlexPHP\Generator\Domain\Writers\TemplateWriter;
+use FlexPHP\Schema\SchemaAttributeInterface;
+use FlexPHP\UseCases\UseCase;
+
+final class CreateTemplateFileUseCase extends UseCase
+{
+    use InflectorTrait;
+
+    /**
+     * Create Template files for action based in entity
+     *
+     * @param CreateTemplateFileRequest $request
+     *
+     * @return CreateTemplateFileResponse
+     */
+    public function execute($request)
+    {
+        $this->throwExceptionIfRequestNotValid(__METHOD__, CreateTemplateFileRequest::class, $request);
+
+        $files = [];
+        $entity = $this->getPluralize($request->entity);
+        $actions = [
+            'index' => 'index.html',
+            'create' => 'new.html',
+            'read' => 'show.html',
+            'update' => 'edit.html',
+            'delete' => '_delete_form.html',
+        ];
+
+        $properties = \array_reduce(
+            $request->attributes,
+            function (array $result, SchemaAttributeInterface $schemaAttribute) {
+                $result[$schemaAttribute->name()] = $schemaAttribute->properties();
+
+                return $result;
+            },
+            []
+        );
+
+        $path = \sprintf('%1$s/../../tmp/skeleton/src/templates/%2$s', __DIR__, $entity);
+
+        foreach ($actions as $action => $filename) {
+            $request = new TemplateBuilder($entity, $action, $properties);
+            $writer = new TemplateWriter($request->build(), $filename, $path);
+            $files[] = $writer->save();
+        }
+
+        return new CreateTemplateFileResponse($files);
+    }
+}
---
+++ tests/Domain/UseCases/CreateTemplateFileUseCaseTest.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of FlexPHP.
+ *
+ * (c) Freddie Gar <freddie.gar@outlook.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FlexPHP\Generator\Tests\Domain\UseCases;
+
+use FlexPHP\Generator\Domain\Messages\Requests\CreateTemplateFileRequest;
+use FlexPHP\Generator\Domain\Messages\Responses\CreateTemplateFileResponse;
+use FlexPHP\Generator\Domain\UseCases\CreateTemplateFileUseCase;
+use FlexPHP\Generator\Tests\TestCase;
+use FlexPHP\Schema\Schema;
+use FlexPHP\UseCases\Exception\NotValidRequestException;
+
+final class CreateTemplateFileUseCaseTest extends TestCase
+{
+    public function testItNotValidRequestThrowException(): void
+    {
+        $this->expectException(NotValidRequestException::class);
+
+        $useCase = new CreateTemplateFileUseCase();
+        $useCase->execute(null);
+    }
+
+    /**
+     * @dataProvider getEntityFile()
+     */
+    public function testItSymfony43Ok(string $schemafile, array $expectedFiles): void
+    {
+        $schema = Schema::fromFile($schemafile);
+
+        $request = new CreateTemplateFileRequest($schema->name(), $schema->attributes());
+
+        $useCase = new CreateTemplateFileUseCase();
+        $response = $useCase->execute($request);
+
+        $this->assertInstanceOf(CreateTemplateFileResponse::class, $response);
+        $this->assertEquals(5, \count($response->files));
+
+        foreach ($response->files as $index => $file) {
+            $filename = \explode('/', $file);
+            $this->assertEquals($expectedFiles[$index], \array_pop($filename));
+            $this->assertFileExists($file);
+
+            \unlink($file);
+        }
+    }
+
+    public function getEntityFile(): array
+    {
+        return [
+            [\sprintf('%1$s/../../Mocks/yaml/posts.yaml', __DIR__), [
+                'index.html.twig',
+                'new.html.twig',
+                'show.html.twig',
+                'edit.html.twig',
+                '_delete_form.html.twig',
+            ]],
+            [\sprintf('%1$s/../../Mocks/yaml/comments.yaml', __DIR__), [
+                'index.html.twig',
+                'new.html.twig',
+                'show.html.twig',
+                'edit.html.twig',
+                '_delete_form.html.twig',
+            ]],
+        ];
+    }
+}
