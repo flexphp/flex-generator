6ae95ba6ac27e0643471fdf286085123c47ba230
Freddie Gar <freddie.gar@outlook.com>
2020-06-27 09:59:51 -0500

    Added create menu file use case

---
+++ src/Domain/UseCases/CreateMenuFileUseCase.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of FlexPHP.
+ *
+ * (c) Freddie Gar <freddie.gar@outlook.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace FlexPHP\Generator\Domain\UseCases;
+
+use FlexPHP\Generator\Domain\Builders\Config\MenuBuilder;
+use FlexPHP\Generator\Domain\Messages\Requests\CreateMenuFileRequest;
+use FlexPHP\Generator\Domain\Messages\Responses\CreateMenuFileResponse;
+use FlexPHP\Generator\Domain\Traits\InflectorTrait;
+use FlexPHP\Generator\Domain\Writers\PhpWriter;
+use FlexPHP\Schema\Schema;
+
+final class CreateMenuFileUseCase
+{
+    use InflectorTrait;
+
+    public function execute(CreateMenuFileRequest $request): CreateMenuFileResponse
+    {
+        $entities = [];
+
+        foreach ($request->schemafiles as $schemafile) {
+            $schema = Schema::fromFile($schemafile);
+            $entities[$schema->name()] = $schema->icon();
+        }
+
+        $entity = new MenuBuilder($entities);
+        $path = \sprintf('%1$s/../../tmp/skeleton/config', __DIR__);
+
+        $writer = new PhpWriter($entity->build(), 'menu', $path);
+
+        return new CreateMenuFileResponse($writer->save());
+    }
+}
---
+++ tests/Domain/UseCases/CreateMenuFileUseCaseTest.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of FlexPHP.
+ *
+ * (c) Freddie Gar <freddie.gar@outlook.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace FlexPHP\Generator\Tests\Domain\UseCases;
+
+use FlexPHP\Generator\Domain\Messages\Requests\CreateMenuFileRequest;
+use FlexPHP\Generator\Domain\Messages\Responses\CreateMenuFileResponse;
+use FlexPHP\Generator\Domain\UseCases\CreateMenuFileUseCase;
+use FlexPHP\Generator\Tests\TestCase;
+
+final class CreateMenuFileUseCaseTest extends TestCase
+{
+    /**
+     * @dataProvider getEntityFiles()
+     *
+     * @param mixed $schemafiles
+     */
+    public function testItOk(array $schemafiles): void
+    {
+        $request = new CreateMenuFileRequest($schemafiles);
+
+        $useCase = new CreateMenuFileUseCase();
+        $response = $useCase->execute($request);
+
+        $this->assertInstanceOf(CreateMenuFileResponse::class, $response);
+        $file = $response->file;
+        $filename = \explode('/', $file);
+        $this->assertEquals('menu.php', \array_pop($filename));
+        $this->assertFileExists($file);
+
+        \unlink($file);
+    }
+
+    public function getEntityFiles(): array
+    {
+        return [
+            [[
+                \sprintf('%1$s/../../Mocks/yaml/posts.yaml', __DIR__),
+                \sprintf('%1$s/../../Mocks/yaml/comments.yaml', __DIR__),
+            ]],
+        ];
+    }
+}
