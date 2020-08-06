630d888198ba4e65e9d0eeaaee6e556b2e6eae27
Freddie Gar <freddie.gar@outlook.com>
2020-08-06 14:43:17 -0500

    Added fk getter builder

---
+++ src/Domain/Builders/Entity/FkGetterBuilder.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of FlexPHP.
+ *
+ * (c) Freddie Gar <freddie.gar@outlook.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace FlexPHP\Generator\Domain\Builders\Entity;
+
+use FlexPHP\Generator\Domain\Builders\AbstractBuilder;
+
+final class FkGetterBuilder extends AbstractBuilder
+{
+    public function __construct(string $name, string $type)
+    {
+        $name = $this->getInflector()->camelProperty($name);
+        $getter = $this->getInflector()->camelProperty($name);
+        $type = $this->getInflector()->entity($type);
+        $typeName = $this->getInflector()->camelProperty($type);
+
+        parent::__construct(\compact('name', 'getter', 'type', 'typeName'));
+    }
+
+    protected function getFileTemplate(): string
+    {
+        return 'FkGetter.php.twig';
+    }
+
+    protected function getPathTemplate(): string
+    {
+        return \sprintf('%1$s/FlexPHP/Entity', parent::getPathTemplate());
+    }
+}
---
+++ tests/Domain/Builders/Entity/FkGetterBuilderTest.php
@@ -0,0 +1,105 @@
+<?php declare(strict_types=1);
+/*
+ * This file is part of FlexPHP.
+ *
+ * (c) Freddie Gar <freddie.gar@outlook.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace FlexPHP\Generator\Tests\Domain\Builders\Entity;
+
+use FlexPHP\Generator\Domain\Builders\Entity\FkGetterBuilder;
+use FlexPHP\Generator\Tests\TestCase;
+
+final class FkGetterBuilderTest extends TestCase
+{
+    public function testItNotRequiredOk(): void
+    {
+        $render = new FkGetterBuilder('foo', 'Foo');
+
+        $this->assertEquals(<<<T
+    public function fooInstance(): ?Foo
+    {
+        return \$this->fooInstance;
+    }
+T
+, $render->build());
+    }
+
+    public function testItRequiredOk(): void
+    {
+        $render = new FkGetterBuilder('foo', 'Foo');
+
+        $this->assertEquals(<<<T
+    public function fooInstance(): ?Foo
+    {
+        return \$this->fooInstance;
+    }
+T
+, $render->build());
+    }
+
+    /**
+     * @dataProvider getEntityNameAndFkGetter
+     */
+    public function testItWithDiffEntityName(
+        string $name, 
+        string $entity, 
+        string $entityExpected,
+        string $getter
+    ): void {
+        $render = new FkGetterBuilder($name, $entity);
+
+        $this->assertEquals(<<<T
+        public function {$getter}Instance(): ?$entityExpected
+        {
+            return \$this->{$name}Instance;
+        }
+    T
+    , $render->build());
+    }
+
+    /**
+     * @dataProvider getPropertyNameAndFkGetter
+     */
+    public function testItWithDiffPropertyName(
+        string $name, 
+        string $type, 
+        string $nameExpected,
+        string $getter
+    ): void {
+        $render = new FkGetterBuilder($name, $type);
+
+        $this->assertEquals(<<<T
+    public function {$getter}Instance(): ?$type
+    {
+        return \$this->{$nameExpected}Instance;
+    }
+T
+, $render->build());
+    }
+
+    public function getEntityNameAndFkGetter(): array
+    {
+        return [
+            ['fk', 'userpassword', 'Userpassword', 'fk'],
+            ['fk', 'USERPASSWORD', 'Userpassword', 'fk'],
+            ['fk', 'UserPassword', 'UserPassword', 'fk'],
+            ['fk', 'userPassword', 'UserPassword', 'fk'],
+            ['fk', 'user_password', 'UserPassword', 'fk'],
+            ['fk', 'Posts', 'Post', 'fk'],
+        ];
+    }
+
+    public function getPropertyNameAndFkGetter(): array
+    {
+        return [
+            ['fooname', 'Foo', 'fooname', 'fooname'],
+            ['FOONAME', 'Foo', 'fooname', 'fooname'],
+            ['FooName', 'Foo', 'fooName', 'fooName'],
+            ['fooName', 'Foo', 'fooName', 'fooName'],
+            ['foo_name', 'Foo', 'fooName', 'fooName'],
+        ];
+    }
+}
